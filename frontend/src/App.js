1e1673426e23bc5bd47a8f686ac161bf460edc99
cahcartiyo <g.havalecz11@gmail.com>
2022-11-20 21:56:14 +0100

    kezelőfelület dune

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
-import {Box,} from "@mui/material"
+import Sidebar from "./pages/Sidebar.jsx"
+import Feed from "./components/Feed.jsx"
+import Rightbar from "./pages/Rightbar.jsx"
+import { Box, createTheme, Stack, ThemeProvider, } from "@mui/material"
 import Navbar from "./components/Navbar.jsx"
+import Add from './pages/Add.jsx'
+import { useState } from "react"
+
+
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  })
   return (
-    <Box>
-      <Navbar/>
-    </Box>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={'background.default'} color={'text.primary'}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode}/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,100 @@
-import styled from '@emotion/styled'
-import { AppBar, Box, InputBase, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
-const StyledToolBar = styled(Toolbar)({
-  display:'felx',
-  justifyContent: 'space-between',
-})
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
 
 const Search = styled("div")(({ theme }) => ({
-  backgroundColor: 'white',
-  padding: '0 10px',
-  borderRadius: '10px',
-  width: '40%'
-}))
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
 
 const Icons = styled(Box)(({ theme }) => ({
-  backgroundColor: 'white',
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
 
-}))
-
-const  Navbar = () => {
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <AppBar position = 'sticky'>
-      <StyledToolBar>
-        <Typography variant='h6'>
-          Chat App
-          </Typography>
-          <Search><InputBase placeholder='search...'/></Search>
-          <Icons>icons</Icons>
-      </StyledToolBar>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+         Chatapp
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://scontent-vie1-1.xx.fbcdn.net/v/t31.18172-8/1147590_718281471515909_3805507_o.jpg?stp=cp0_dst-jpg_e15_q65_s320x320&_nc_cat=101&ccb=1-7&_nc_sid=e007fa&_nc_ohc=awwdpognY-8AX9camfP&_nc_ht=scontent-vie1-1.xx&oh=00_AfA_-n8-z9478LKxHGWenDQRfFk3YC6i6uTjxH6x1P5xjA&oe=639E474D'
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://scontent-vie1-1.xx.fbcdn.net/v/t31.18172-8/1147590_718281471515909_3805507_o.jpg?stp=cp0_dst-jpg_e15_q65_s320x320&_nc_cat=101&ccb=1-7&_nc_sid=e007fa&_nc_ohc=awwdpognY-8AX9camfP&_nc_ht=scontent-vie1-1.xx&oh=00_AfA_-n8-z9478LKxHGWenDQRfFk3YC6i6uTjxH6x1P5xjA&oe=639E474D'
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem component='a' href='#profile'>Profile</MenuItem>
+        <MenuItem component='a' href='#logout'>Logout</MenuItem>
+      </Menu>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
 
